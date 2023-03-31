d7a629a80485539b0e1c0f878ced3a56579047d8
gmovia <gmovia@fi.uba.ar>
2023-03-31 13:58:08 -0300

    agrego ubicacion y modifico eventos

--- src/schemas/event.py
+++ src/schemas/event.py
@@ -2,13 +2,17 @@ from pydantic import BaseModel
 from typing import Optional, List
 from datetime import date
 from src.schemas.image import ImageSchema
+from src.schemas.ubication import UbicationSchema
 
 class EventSchema(BaseModel):
+    title: str
+    category: str
     images: List[ImageSchema]
     date: date
     description: str
     tickets: int 
-
+    ubication: UbicationSchema
+    
     class Config:
         orm_mode = True
 
