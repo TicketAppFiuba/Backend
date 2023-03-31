a240fad97ce1982ab016d45fe9d8cb376f6182bd
gmovia <gmovia@fi.uba.ar>
2023-03-31 13:48:49 -0300

    agrego funciones en event

--- src/config/event.py
+++ src/config/event.py
@@ -4,10 +4,21 @@ from src.models.event import Event
 from src.models.image import Image
 
 def create(event: EventSchema, email: str, db: Session):
-    event_db = Event(**event.dict(exclude={'images'}), organizer_email=email)
+    event_db = Event(**event.dict(exclude={'images', 'ubication'}),
+                     direction=event.ubication.direction,
+                     latitude = event.ubication.latitude,
+                     length = event.ubication.length, 
+                     organizer_email=email)
     for img in event.images:
-        event_db.images.append(Image(link=img.link, event_id=event_db.id))
+        event_db.images.append(Image(link=img.link, event_id=event_db.id))        
     db.add(event_db)
     db.commit()
     db.refresh(event_db)
     return event_db
+
+def delete(event_db: Event, db: Session):
+    db.delete(event_db)
+    db.commit()
+
+def get(event_id: int, db: Session):
+    return db.query(Event).filter(Event.id == event_id).first()
