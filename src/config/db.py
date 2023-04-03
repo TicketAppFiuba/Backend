187af537792b3ba9a08581c6d95dc926555f8ea3
JulioCastillo1 <jecastillo@fi.uba.ar>
2023-04-02 22:29:53 -0300

    Agrego carga de variables de entorno, modifico url

--- src/config/db.py
+++ src/config/db.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-engine = create_engine("sqlite:///./sql_app.db", connect_args={"check_same_thread": False})
+db_url = os.getenv("DATABASE_URL")
+db_port = os.getenv("DATABASE_PORT")
+database = os.getenv("DATABASE")
+db_username = os.getenv("DATABASE_USERNAME")
+db_password = os.getenv("DATABASE_PASSWORD")
+
+engine = create_engine(url="postgresql://{0}:{1}@{2}:{3}/{4}".format(db_username, db_password, db_url, db_port, database))
+
+#engine = create_engine("sqlite:///./sql_app.db", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
