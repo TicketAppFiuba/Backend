b8562631e471d0269ded6a3e15c248bf7da820f4
JulioCastillo1 <jecastillo@fi.uba.ar>
2023-04-04 10:21:32 -0300

    Actualizo cors middleware

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from fastapi import FastAPI
 from src.routes import organizer_event, user_access, organizer_access, images, user_event
 from starlette.middleware.sessions import SessionMiddleware
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title = "TicketAPP")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_access.router)
 app.include_router(organizer_access.router)
 app.include_router(organizer_event.router)
