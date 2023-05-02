ca017e260555918b2c97f5dddd348a65bb67ba6a
gmovia <gmovia@fi.uba.ar>
2023-05-02 18:50:59 -0300

    pongo esquema de salida

--- src/schemas/authorizer.py
+++ src/schemas/authorizer.py
@@ -1,7 +1,25 @@
 from pydantic import BaseModel
+from datetime import date
 
 class AuthorizerSchema(BaseModel):
     email: str
+
+    class Config:
+        orm_mode = True
+
+class EventOutSchema(BaseModel):
+    id: int
+    title: str
+    direction: str
+    capacity: int
+    latitude: float
+    organizer_email: str
+    description: str
+    category: str
+    date: date
+    vacancies: int
+    longitude: float
+    link: str
 
     class Config:
         orm_mode = True
