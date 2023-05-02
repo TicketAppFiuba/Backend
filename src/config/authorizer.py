6e6c7effc6c293f1ac8e46d0d3aeb98ff85c1f44
gmovia <gmovia@fi.uba.ar>
2023-05-02 18:39:38 -0300

    agrego link

--- src/config/authorizer.py
+++ src/config/authorizer.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from src.schemas.user import UserSchema
 from src.models.event_authorizer import EventAuthorizer
 from src.models.event import Event
+from src.models.image import Image
 
 def create(user: UserSchema, db: Session):
     user_db = Authorizer(email=user.email, name=user.name, login=True)
@@ -22,7 +23,7 @@ def get(email: str, db: Session):
     return db.query(Authorizer).filter(Authorizer.email == email).first()
 
 def getAllEvents(auth_email: str, db: Session):
-    return db.query(Event).filter(EventAuthorizer.email == auth_email).filter(EventAuthorizer.event_id == Event.id).all()
+    return db.query(Event, Image.link).filter(EventAuthorizer.email == auth_email).filter(EventAuthorizer.event_id == Event.id).filter(Event.pic_id == Image.id).all()
 
 def canScan(auth_email: str, event_id: int, db: Session):
     return db.query(EventAuthorizer).filter(EventAuthorizer.event_id == event_id).filter(EventAuthorizer.email == auth_email).first() is not None
