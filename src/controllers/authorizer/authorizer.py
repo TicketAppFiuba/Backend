d65a5c608754e19e22ffd5260bc24813138c0771
gmovia <gmovia@fi.uba.ar>
2023-05-03 13:20:38 -0300

    refactorizo validaciones

--- src/controllers/authorizer/authorizer.py
+++ src/controllers/authorizer/authorizer.py
@@ -1,4 +1,3 @@
-from fastapi import APIRouter, HTTPException
 from sqlalchemy.orm import Session
 from src.models.authorizer import Authorizer
 from src.models.event import Event
@@ -7,17 +6,10 @@ from src.config import authorizer
 from src.config import image
 from src.schemas.qr import *
 from src.schemas.authorizer import EventOutSchema
+from src.controllers.authorizer import validations
 
 def authorize(qr: QRSchema, authorizer_db: Authorizer, db: Session):
-    reservation_db = reservation.get(qr.reservation_code, db)
-    if reservation_db is None:
-        raise HTTPException(status_code=404, detail="La reserva no existe.")
-    if authorizer.canScan(authorizer_db.email, reservation_db.event_id, db) is False:
-        raise HTTPException(status_code=403, detail="El autorizador no tiene permisos para escanear.")
-    if reservation_db.event_id != qr.event_id:
-        raise HTTPException(status_code=403, detail="La reserva no coincide con el evento.")
-    if reservation_db.scanned == True:
-        raise HTTPException(status_code=403, detail="El codigo ya fue escaneado.")
+    reservation_db = validations.check_scan_reservation(qr, authorizer_db, db)
     reservation.scan(reservation_db, db)
     return {"detail": "La reserva fue escaneada correctamente."} 
 
--- src/controllers/user/validations.py
+++ src/controllers/user/validations.py
@@ -4,6 +4,7 @@ from src.config import reservation
 from src.config import event
 from src.config import complaint
 from src.models.user import User
+from src.models.event import Event
 from src.schemas.reservation import *
 from src.schemas.complaint import *
 
@@ -12,23 +13,30 @@ def validate_event(event_id: int, db: Session):
     if event_db is None:
         raise HTTPException(status_code=404, detail="Event not exist.")
     return event_db
-
-def check_permission_reservation(reservationSchema: ReservationCreateSchema, user_db: User, db: Session):
-    event_db = validate_event(reservationSchema.event_id, db)
-    reservation_db = reservation.getByUserAndEvent(user_db.id, event_db.id, db)
+    
+def validate_reservation(schema: ReservationSchema, db: Session):
+    reservation_db = reservation.getByUserAndEvent(schema.user_id, schema.event_id, db)
     if reservation_db is not None:
         raise HTTPException(status_code=403, detail="Event already booked.")
-    if reservationSchema.tickets > 4:
-        raise HTTPException(status_code=403, detail="The maximum number of tickets is four.")
-    if reservationSchema.tickets <= 0:
-        raise HTTPException(status_code=403, detail="The number of tickets must be greater than zero.")
-    if event_db.vacancies - reservationSchema.tickets < 0:
-        raise HTTPException(status_code=403, detail="The number of tickets exceeds the capacity of the event.")
-    return ReservationSchema(event_id=reservationSchema.event_id, user_id=user_db.id, tickets=reservationSchema.tickets)
+    return schema 
 
-def check_permission_complaint(complaintSchema: ComplaintSchema, user_db: User, db: Session):
-    event_db = validate_event(complaintSchema.event_id, db)
-    complaint_db = complaint.getByUserAndEvent(user_db.id, event_db.id, db)
+def validate_complaint(schema: ComplaintSchema, db: Session):
+    complaint_db = complaint.getByUserAndEvent(schema.user_id, schema.event_id, db)
     if complaint_db is not None:
         raise HTTPException(status_code=403, detail="Complaint already report.")
-    return ComplaintSchema(event_id=complaintSchema.event_id, user_id=user_db.id, category=complaintSchema.category, description=complaintSchema.description)
+    return schema    
+
+def validate_tickets(tickets: int, event_db: Event):
+    if tickets > 4 or tickets <= 0:
+        raise HTTPException(status_code=403, detail="The number of tickets is invalid.")
+    if event_db.vacancies - tickets < 0:
+        raise HTTPException(status_code=403, detail="The number of tickets exceeds the capacity of the event.")
+    
+def check_permission_reservation(reservationSchema: ReservationCreateSchema, user_db: User, db: Session):
+    event_db = validate_event(reservationSchema.event_id, db)
+    validate_tickets(reservationSchema.tickets, event_db)
+    return validate_reservation(ReservationSchema(**reservationSchema.dict(), user_id=user_db.id), db)
+
+def check_permission_complaint(complaintSchema: ComplaintCreateSchema, user_db: User, db: Session):
+    validate_event(complaintSchema.event_id, db)
+    return validate_complaint(ComplaintSchema(**complaintSchema.dict(), user_id=user_db.id), db)
