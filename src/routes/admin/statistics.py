552096808746a907b51da3f8c11fd695656eba89
gmovia <gmovia@fi.uba.ar>
2023-05-09 14:08:22 -0300

    agrego autenticacion x jwt en backoffice de adm

--- src/routes/admin/statistics.py
+++ src/routes/admin/statistics.py
@@ -3,13 +3,14 @@ from sqlalchemy.orm import Session
 from src.config.db import get_db
 from src.schemas.complaint import *
 from src.config.complaint import *
+from src.controllers.admin.access import verify
 
 adm = APIRouter(tags=["Admin | Statistics"])
 
 @adm.get("/admin/complaints/users/ranking", status_code=200)
-def user_ranking(db: Session = Depends(get_db)):
+def user_ranking(admin: str = Depends(verify), db: Session = Depends(get_db)):
     return getRankingFromUsers(db)
 
 @adm.get("/admin/complaints/events/ranking", status_code=200)
-def event_ranking(db: Session = Depends(get_db)):
+def event_ranking(admin: str = Depends(verify), db: Session = Depends(get_db)):
     return getRankingFromEvents(db)
