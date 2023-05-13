858ca4be2584a389d402a6697277f3a92e1e1c76
gmovia <gmovia@fi.uba.ar>
2023-05-13 20:00:38 -0300

    agrego imagen de portada -  reserva

--- src/controllers/user/reservation.py
+++ src/controllers/user/reservation.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from src.config import reservation
 from src.controllers.user import permissions
 from src.controllers.user import event
+from src.config import image
 
 def create_reservation(reservationSchema: ReservationCreateSchema, user_db: User, db: Session):
     reservationSchema = permissions.check_create_reservation(reservationSchema, user_db, db)
@@ -12,4 +13,10 @@ def create_reservation(reservationSchema: ReservationCreateSchema, user_db: User
     return {"detail": "Reservation created successfully", "code": reservation_db.code}
 
 def get_reservations_from_user(user_db: User, db: Session):
-    return reservation.getAllFromUser(user_db.id, db)
+    reservation_list = []
+    reservations = reservation.getAllFromUser(user_db.id, db)
+    for res in reservations:
+        if res["Event"].pic_id != None:
+            res["Event"].pic_id = image.getCoverImage(res["Event"].pic_id, db)
+        reservation_list.append(res)
+    return reservation_list
