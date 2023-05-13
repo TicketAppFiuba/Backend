d7c1467061faf256e3c71c4b6a8bb08b5356f761
gmovia <gmovia@fi.uba.ar>
2023-05-12 22:17:02 -0300

    cambio nombre funcion

--- src/routes/admin/complaints.py
+++ src/routes/admin/complaints.py
@@ -17,5 +17,5 @@ def complaints(category: str | None = None,
     return complaint.get_complaints(query, db)
 
 @adm_complaint.get("/admin/event/complaints", status_code=200)
-def complaints(event_id: int, admin: str = Depends(verify), db: Session = Depends(get_db)):
+def complaints_by_event(event_id: int, admin: str = Depends(verify), db: Session = Depends(get_db)):
     return complaint.get_complaints_by_event(event_id, db)
