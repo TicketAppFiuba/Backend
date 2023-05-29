161f3d9572e47cebebf2e02f1effc3e70e243acc
gmovia <gmovia@fi.uba.ar>
2023-05-29 13:10:18 -0300

    agrego hilo para verificar si el evento finalizo

--- date_process.py
+++ date_process.py
@@ -2,12 +2,15 @@ import datetime
 from src.models.event import Event
 from src.config.db import SessionLocal
 from src.config.event import *
+import time
 
-def change_status_to_finalished():
+def change_status_to_finalished(stop_flag):
     db = SessionLocal()
-    events = db.query(Event).filter(Event.state == "published").all()
-    for event_db in events:
-        if datetime.datetime.now() > event_db.date:
-            finish(event_db, db)
+    while not stop_flag.is_set():
+        events = db.query(Event).filter(Event.state == "published").all()
+        for event_db in events:
+            if datetime.datetime.now() > event_db.date:
+                finish(event_db, db)
+                time.sleep(60)
     db.close()
 
