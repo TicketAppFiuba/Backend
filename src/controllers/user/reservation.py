bbc531bc12057cc2a533307fce7af4591c015319
gmovia <gmovia@fi.uba.ar>
2023-06-03 15:00:45 -0300

    agrego codigo en user/reservations

--- src/controllers/user/reservation.py
+++ src/controllers/user/reservation.py
@@ -6,6 +6,7 @@ from src.controllers.user import permissions
 from src.controllers.user import event
 from src.config import image, notifications, favorite
 from src.schemas.event import *
+from src.schemas.reservation import *
 from src.models.event import Event
 
 def create_reservation(reservationSchema: ReservationCreateSchema, user_db: User, db: Session):
@@ -19,13 +20,14 @@ def get_reservations_from_user(user_db: User, db: Session):
     reservation_list = []
     reservations = reservation.getAllFromUser(user_db.id, db)
     for res in reservations:
-        reservation_list.append(get_event_with_cover_pic(res["Event"], user_db, db))
+        reservation_list.append(get_reservation(res, user_db, db))
     return reservation_list
 
-def get_event_with_cover_pic(event: Event, user: User, db: Session):
-    event_with_cover_pic = EventSchemaOutWithLink.from_orm(event)
-    cover_image = image.getCoverImage(event.pic_id, db)
+def get_reservation(res, user: User, db: Session):
+    reservation_schema = ReservationOutSchema.from_orm(res["Event"])
+    cover_image = image.getCoverImage(res["Event"].pic_id, db)
     if cover_image is not None:
-        event_with_cover_pic.link = cover_image.link
-    event_with_cover_pic.favorite = (favorite.getByUserAndEvent(user.id, event.id, db) != None)
-    return event_with_cover_pic
+        reservation_schema.link = cover_image.link
+    reservation_schema.favorite = (favorite.getByUserAndEvent(user.id, res["Event"].id, db) != None)
+    reservation_schema.code = res["Reservation"].code
+    return reservation_schema
